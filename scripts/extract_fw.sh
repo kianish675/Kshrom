22273107acd490518a7cf2c221d52c7879d92aed
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-10 14:48:38 +0300

    unica: Remove the need of a target firmware
    There's not much need for it

--- scripts/extract_fw.sh
+++ scripts/extract_fw.sh
@@ -201,7 +201,14 @@ EXTRACT_ALL()
     echo ""
 }
 
-FIRMWARES=( "$SOURCE_FIRMWARE" "$TARGET_FIRMWARE" )
+FIRMWARES=( "$SOURCE_FIRMWARE" )
+IFS=':' read -a SOURCE_EXTRA_FIRMWARES <<< "$TARGET_FIRMWARE"
+if [ "${#TARGET_FIRMWARE[@]}" -ge 1 ]; then
+    for i in "${TARGET_FIRMWARE[@]}"
+    do
+        FIRMWARES+=( "$i" )
+    done
+fi
 IFS=':' read -a SOURCE_EXTRA_FIRMWARES <<< "$SOURCE_EXTRA_FIRMWARES"
 if [ "${#SOURCE_EXTRA_FIRMWARES[@]}" -ge 1 ]; then
     for i in "${SOURCE_EXTRA_FIRMWARES[@]}"
