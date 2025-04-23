9bc0723c277e627618e74548e395a33e611a62fe
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-23 12:15:36 +0300

    unica: patches: vndk: Hardcode VNDK30 application
    Our device is VNDK30. There's no need to load the rest

--- unica/patches/vndk/customize.sh
+++ unica/patches/vndk/customize.sh
@@ -1,27 +1,3 @@
-if [[ "$SOURCE_VNDK_VERSION" != "$TARGET_VNDK_VERSION" ]]; then
-    if $TARGET_HAS_SYSTEM_EXT; then
-        SYS_EXT_DIR="$WORK_DIR/system_ext"
-    else
-        SYS_EXT_DIR="$WORK_DIR/system/system/system_ext"
-    fi
+ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
+sed -i "s/version>$SOURCE_VNDK_VERSION/version>$TARGET_VNDK_VERSION/g" "$WORK_DIR/system/system/system_ext/etc/vintf/manifest.xml"
 
-    if [ ! -f "$SYS_EXT_DIR/apex/com.android.vndk.v$TARGET_VNDK_VERSION.apex" ]; then
-        DELETE_FROM_WORK_DIR "system_ext" "apex/com.android.vndk.v$SOURCE_VNDK_VERSION.apex"
-        case "$TARGET_VNDK_VERSION" in
-            "30")
-                ADD_TO_WORK_DIR "a73xqxx" "system_ext" "apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-            "31")
-                ADD_TO_WORK_DIR "r0qxxx" "system_ext" "apex/com.android.vndk.v31.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-            "33")
-                ADD_TO_WORK_DIR "dm3qxxx" "system_ext" "apex/com.android.vndk.v33.apex" 0 0 644 "u:object_r:system_file:s0"
-                ;;
-        esac
-        sed -i "s/version>$SOURCE_VNDK_VERSION/version>$TARGET_VNDK_VERSION/g" "$SYS_EXT_DIR/etc/vintf/manifest.xml"
-    else
-        echo "VNDK v$TARGET_VNDK_VERSION apex is already in place. Ignoring"
-    fi
-else
-    echo "SOURCE_VNDK_VERSION and TARGET_VNDK_VERSION are the same. Ignoring"
-fi
