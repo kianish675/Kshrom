8bf0b747e8946d0399ffd602610dfc21d2ac134f
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-26 20:11:58 +0300

    a70q: Typo fixes for product_feature_lite

--- target/a70q/patches/product_feature_lite/customize.sh
+++ target/a70q/patches/product_feature_lite/customize.sh
@@ -4,7 +4,6 @@ DECODE_APK "system/framework/ssrm.jar"
 DECODE_APK "system/priv-app/SecSettings/SecSettings.apk"
 DECODE_APK "system/framework/framework.jar"
 DECODE_APK "system/framework/gamemanager.jar"
-DECODE_APK "system/framework/gamesdk.jar"
 DECODE_APK "system/framework/secinputdev-service.jar"
 DECODE_APK "system/priv-app/SettingsProvider/SettingsProvider.apk"
 DECODE_APK "system_ext/priv-app/SystemUI/SystemUI.apk"
@@ -13,9 +12,8 @@ echo "Applying MAINLINE_API_LEVEL patches"
 FTP="
 system/framework/esecomm.jar/smali/com/sec/esecomm/EsecommAdapter.smali
 system/framework/services.jar/smali/com/android/server/SystemServer.smali
-system/framework/services.jar/smali_classes2/com/android/server/enterprise/hdm/HdmVendorController.smali
-system/framework/services.jar/smali_classes2/com/android/server/knox/dar/ddar/ta/TAProxy.smali
-system/framework/services.jar/smali_classes3/com/android/server/power/PowerManagerUtil.smali
+system/framework/services.jar/smali/com/android/server/enterprise/hdm/HdmVendorController.smali
+system/framework/services.jar/smali_classes2/com/android/server/power/PowerManagerUtil.smali
 system/framework/services.jar/smali_classes2/com/android/server/sepunion/EngmodeService\$EngmodeTimeThread.smali
 "
 for f in $FTP; do
@@ -34,15 +32,13 @@ for f in $FTP; do
 sed -i "s/\"$SOURCE_MDNIE_SUPPORTED_MODES\"/\"$TARGET_MDNIE_SUPPORTED_MODES\"/g" "$APKTOOL_DIR/$f"
 sed -i "s/\"$SOURCE_MDNIE_WEAKNESS_SOLUTION_FUNCTION\"/\"$TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION\"/g" "$APKTOOL_DIR/$f"
 done
-fi
 
 
 echo "Applying HFR_MODE patches"
 FTP="
-system/framework/framework.jar/smali_classes5/com/samsung/android/rune/CoreRune.smali
+system/framework/framework.jar/smali_classes6/com/samsung/android/rune/CoreRune.smali
 system/framework/framework.jar/smali_classes6/com/samsung/android/hardware/display/RefreshRateConfig.smali
 system/framework/gamemanager.jar/smali/com/samsung/android/game/GameManagerService.smali
-system/framework/gamesdk.jar/smali/com/samsung/android/gamesdk/vrr/GameSDKVrrManager.smali
 system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/SemInputDeviceManagerService.smali
 system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/SemInputFeatures.smali
 system/framework/secinputdev-service.jar/smali/com/samsung/android/hardware/secinputdev/SemInputFeaturesExtra.smali
@@ -53,7 +49,6 @@ system_ext/priv-app/SystemUI/SystemUI.apk/smali/com/android/systemui/LsRune.smal
 for f in $FTP; do
 sed -i "s/\"$SOURCE_HFR_MODE\"/\"$TARGET_HFR_MODE\"/g" "$APKTOOL_DIR/$f"
 done
-fi
 
 
 echo "Applying HFR_SUPPORTED_REFRESH_RATE patches"
@@ -68,7 +63,6 @@ else
 sed -i "s/\"$SOURCE_HFR_SUPPORTED_REFRESH_RATE\"/\"\"/g" "$APKTOOL_DIR/$f"
 fi
 done
-fi
 
 
 echo "Applying SemMultiMicManager patches"
@@ -78,8 +72,6 @@ system/framework/framework.jar/smali_classes5/com/samsung/android/camera/mic/Sem
 for f in $FTP; do
 sed -i "s/$SOURCE_MULTI_MIC_MANAGER_VERSION/$TARGET_MULTI_MIC_MANAGER_VERSION/g" "$APKTOOL_DIR/$f"
 done
-fi
-
 
 echo "Applying SSRM patches"
 FTP="
@@ -88,7 +80,6 @@ system/framework/ssrm.jar/smali/com/android/server/ssrm/Feature.smali
 for f in $FTP; do
 sed -i "s/$SOURCE_SSRM_CONFIG_NAME/$TARGET_SSRM_CONFIG_NAME/g" "$APKTOOL_DIR/$f"
 done
-fi
 
 
 echo "Applying model detection patches"
@@ -99,4 +90,3 @@ system/framework/services.jar/smali/com/android/server/am/FreecessController.sma
 for f in $FTP; do
 sed -i "s/ro\.product\.model/ro\.product\.vendor\.model/g" "$APKTOOL_DIR/$f"
 done
-fi
