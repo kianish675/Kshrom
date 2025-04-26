c78e08fee641f1510ebd693d4be408080a0b6e27
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-26 20:27:44 +0300

    a70q: fixed saiv generation

--- target/a70q/patches/saiv/customize.sh
+++ target/a70q/patches/saiv/customize.sh
@@ -1,3 +1,4 @@
+SKIP_UNZIP=1
 SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
 
 echo "Deleting base saiv"
@@ -5,6 +6,6 @@ rm -r $WORK_DIR/system/system/saiv
 rm -r $WORK_DIR/system/system/etc/saiv
 
 echo "Adding target saiv"
-cp -r $SRC_DIR/target/a70q/patches/saiv/system $WORK_DIR/system/system
+cp -r $SRC_DIR/target/a70q/patches/saiv/system/* $WORK_DIR/system/system
 
 echo "saiv was successfully patched!"
