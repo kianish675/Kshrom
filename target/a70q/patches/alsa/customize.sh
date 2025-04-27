b902501c48b9df2a82629d203702e1b8aab1fa4a
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-27 11:03:23 +0300

    a70q: Let the build system do the ALSA installation automatically

--- target/a70q/patches/alsa/customize.sh
+++
@@ -1,6 +0,0 @@
-SOURCE_FIRMWARE_PATH="$FW_DIR/$(echo -n "$SOURCE_FIRMWARE" | sed 's./._.g' | rev | cut -d "_" -f2- | rev)"
-
-echo "Adding target ALSA configuration"
-cp -r $SRC_DIR/target/a70q/patches/alsa/alsa $WORK_DIR/system/system/usr/share
-
-echo "ALSA configuration was successfully patched!"
