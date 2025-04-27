2794aba523c764f5bbfcfda4febacf399d5b8714
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-27 18:00:40 +0300

    unica: add a context hack for base related issues

---
+++ unica/patches/context_hack/customize.sh
@@ -0,0 +1,3 @@
+SET_METADATA "system" "system/etc/aer_product_name.xml" 0 0 644 "u:object_r:system_file:s0"
+SET_METADATA "system" "system/lib64/libvm_control_product.dylib.so" 0 0 644 "u:object_r:system_file:s0"
+SET_METADATA "system" "system/system_ext/etc/permissions/embms-noship_product_privapp_permissions_qti.xml" 0 0 644 "u:object_r:system_file:s0"
