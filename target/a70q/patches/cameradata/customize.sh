93e89bca485554e2a9409e1b791122e9f7d1cfc7
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-30 09:18:45 +0300

    a70q: Implement UN1CA's fix for AI Photo Editor

--- target/a70q/patches/cameradata/customize.sh
+++ target/a70q/patches/cameradata/customize.sh
@@ -6,4 +6,15 @@ rm -r $WORK_DIR/system/system/cameradata
 echo "Adding target cameradata"
 cp -r $SRC_DIR/target/a70q/patches/cameradata/system/cameradata $WORK_DIR/system/system
 
+echo "Fix AI Photo Editor"
+cp -a --preserve=all \
+    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/unica_bokeh_feature.json"
+SET_METADATA "system" "system/cameradata/portrait_data/unica_bokeh_feature.json" 0 0 644 "u:object_r:system_file:s0"
+sed -i "s/MODEL_TYPE_INSTANCE_CAPTURE/MODEL_TYPE_OBJ_INSTANCE_CAPTURE/g" \
+    "$WORK_DIR/system/system/cameradata/portrait_data/single_bokeh_feature.json"
+sed -i \
+    's/system\/cameradata\/portrait_data\/single_bokeh_feature.json/system\/cameradata\/portrait_data\/unica_bokeh_feature.json\x00/g' \
+    "$WORK_DIR/system/system/lib64/libPortraitSolution.camera.samsung.so"
+
 echo "cameradata was successfully patched!"
