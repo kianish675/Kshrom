32c2d7feea186d9b69af32b509cd8c3ac64e5b90
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-04-30 11:26:45 +0300

    a70q: Minor fixes on the miscs patch

--- target/a70q/patches/miscs/customize.sh
+++ target/a70q/patches/miscs/customize.sh
@@ -1,7 +1,7 @@
 echo "Fix up /product/etc/build.prop"
 sed -i "/# Removed by /d" "$WORK_DIR/system/system/product/etc/build.prop" \
     && sed -i "s/#bluetooth./bluetooth./g" "$WORK_DIR/system/system/product/etc/build.prop" \
-    && sed -i "s/?=/=/g" "$WORK_DIR/product/etc/build.prop" \
+    && sed -i "s/?=/=/g" "$WORK_DIR/system/system/product/etc/build.prop" \
     && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/system/system/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/system/system/product/etc/build.prop"
 
 echo "Remove unsupported permissions"
