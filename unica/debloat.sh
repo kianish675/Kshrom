c5bb20d66342496539e1518d7dfaf708a57048a9
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-05-07 19:15:23 +0300

    unica: Clear out the  debloat list
    Some enlisted applications are not included withing the base firmware and result in multiple warnings withing the build system.

--- unica/debloat.sh
+++ unica/debloat.sh
@@ -24,33 +24,10 @@
 SYSTEM_DEBLOAT+="
 dpolicy_system
 "
-VENDOR_DEBLOAT+="
-etc/dpolicy
-"
-
-# Samsung SIM Unlock
-SYSTEM_DEBLOAT+="
-system/bin/ssud
-system/etc/init/ssu_a36xqnsxx.rc
-system/etc/init/ssu.rc
-system/etc/permissions/privapp-permissions-com.samsung.ssu.xml
-system/etc/sysconfig/samsungsimunlock.xml
-system/lib64/android.security.securekeygeneration-ndk.so
-system/lib64/libssu_keystore2.so
-system/priv-app/SsuService
-"
-
-# Recovery restoration script
-VENDOR_DEBLOAT+="
-recovery-from-boot.p
-bin/install-recovery.sh
-etc/init/vendor_flash_recovery.rc
-"
 
 # Apps debloat
 PRODUCT_DEBLOAT+="
 app/Chrome64
-app/DuoStub
 app/Duo
 app/Gmail2
 app/Maps
@@ -62,7 +39,6 @@ priv-app/SearchSelector
 SYSTEM_DEBLOAT+="
 system/app/AutomationTest_FB
 system/app/DRParser
-system/app/DictDiotekForSec
 system/app/FactoryAirCommandManager
 system/app/FactoryCameraFB
 system/app/FBAppManager_NS
@@ -70,33 +46,26 @@ system/app/HMT
 system/app/MAPSAgent
 system/app/MoccaMobile
 system/app/PlayAutoInstallConfig
-system/app/SamsungCalendar
 system/app/SamsungPassAutofill_v1
 system/app/SamsungTTSVoice_de_DE_f00
 system/app/SamsungTTSVoice_en_GB_f00
 system/app/SamsungTTSVoice_en_US_l03
 system/app/SamsungTTSVoice_es_ES_f00
-system/app/SamsungTTSVoice_es_MX_f00
 system/app/SamsungTTSVoice_es_US_f00
 system/app/SamsungTTSVoice_fr_FR_f00
 system/app/SamsungTTSVoice_hi_IN_f00
 system/app/SamsungTTSVoice_it_IT_f00
 system/app/SamsungTTSVoice_pl_PL_f00
-system/app/SamsungTTSVoice_pt_BR_f00
 system/app/SamsungTTSVoice_ru_RU_f00
 system/app/SamsungTTSVoice_th_TH_f00
 system/app/SamsungTTSVoice_vi_VN_f00
 system/app/SilentLog
-system/app/SmartReminder
-system/app/WebManual
 system/app/WlanTest
 system/etc/init/samsung_pass_authenticator_service.rc
 system/etc/permissions/privapp-permissions-com.microsoft.skydrive.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.app.kfa.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.authfw.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.carkey.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.dkey.xml
-system/etc/permissions/privapp-permissions-com.samsung.android.game.gamehome.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.providers.factory.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.samsungpass.xml
 system/etc/permissions/privapp-permissions-com.samsung.android.spayfw.xml
@@ -106,9 +75,8 @@ system/etc/permissions/privapp-permissions-com.sec.android.soagent.xml
 system/etc/permissions/privapp-permissions-com.sec.bcservice.xml
 system/etc/permissions/privapp-permissions-com.sec.epdgtestapp.xml
 system/etc/permissions/privapp-permissions-com.sec.facatfunction.xml
-system/etc/permissions/privapp-permissions-com.sem.factoryapp.xmlz
+system/etc/permissions/privapp-permissions-com.sem.factoryapp.xml
 system/etc/permissions/privapp-permissions-com.wssyncmldm.xml
-system/etc/permissions/privapp-permissions-de.axelspringer.yana.zeropage.xml
 system/etc/permissions/privapp-permissions-meta.xml
 system/etc/sysconfig/digitalkey.xml
 system/etc/sysconfig/meta-hiddenapi-package-allowlist.xml
@@ -132,7 +100,6 @@ system/priv-app/FBServices
 system/priv-app/FacAtFunction
 system/priv-app/FactoryTestProvider
 system/priv-app/FotaAgent
-system/priv-app/GameHome
 system/priv-app/ModemServiceMode
 system/priv-app/OneDrive_Samsung_v3
 system/priv-app/PaymentFramework
@@ -143,12 +110,11 @@ system/priv-app/SamsungCarKeyFw
 system/priv-app/SamsungPass
 system/priv-app/SmartEpdgTestApp
 system/priv-app/StickerFaceARAvatar
-system/priv-app/Upday
 "
 
+# Samsung ESIM Stack
 SYSTEM_DEBLOAT+="
 system/etc/permissions/privapp-permissions-com.samsung.android.app.esimkeystring.xml
-system/etc/permissions/privapp-permissions-com.samsung.euicc.xml
 system/etc/permissions/privapp-permissions-com.samsung.euicc.mep.xml
 system/etc/sysconfig/preinstalled-packages-com.samsung.android.app.esimkeystring.xml
 system/etc/sysconfig/preinstalled-packages-com.samsung.euicc.xml
@@ -156,7 +122,7 @@ system/priv-app/EsimKeyString
 system/priv-app/EuiccService
 "
 
-
+# Knox Matrix
 SYSTEM_DEBLOAT+="
 system/bin/fabric_crypto
 system/etc/init/fabric_crypto.rc
