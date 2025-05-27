8d25387253a70fb23c9fdc57f65f7daf704735d8
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-05-27 20:44:25 +0300

    unica: Abolish vndk
    It's task is now performed by legacy_stack

--- unica/patches/vndk/customize.sh
+++
@@ -1,9 +0,0 @@
-# 202404 base so create an APEX folder in system_ext
-{
-mkdir $WORK_DIR/system/system/system_ext/apex
-} || {
-echo "apex folder already exists"
-}
-
-ADD_TO_WORK_DIR "a73xq" "system" "system_ext/apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
-echo "VNDK30 was applied successfully!"
