1c8a33bdbbeedf363bb71c4bd0249ae6dabc78c1
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-05-27 20:43:29 +0300

    unica: patches: Goodbye Desixtyfication!
    Transition all the workload to be done by the new Legacy Stack Application

---
+++ unica/patches/legacy_stack/customize.sh
@@ -0,0 +1,13 @@
+{
+mkdir $WORK_DIR/system/system/system_ext/apex
+} || {
+echo "apex folder already exists"
+}
+
+echo "Applying Legacy stack"
+ADD_TO_WORK_DIR "a73xq" "system" "apex" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "bin" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "lib" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "lib64" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "system_ext/apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
+echo "Legacy stack was applied successfully!"
