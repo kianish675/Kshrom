e91f990b5e6f2dd578c2732eeb935067f0f40984
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-05-27 21:00:25 +0300

    unica: patches: legacy_stack: Fix bad redundancies

--- unica/patches/legacy_stack/customize.sh
+++ unica/patches/legacy_stack/customize.sh
@@ -5,8 +5,8 @@ echo "apex folder already exists"
 }
 
 echo "Applying Legacy stack"
-ADD_TO_WORK_DIR "a73xq" "system" "apex" 0 0 755 "u:object_r:system_lib_file:s0"
-ADD_TO_WORK_DIR "a73xq" "system" "bin" 0 0 755 "u:object_r:system_lib_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "apex" 0 0 755 "u:object_r:system_file:s0"
+ADD_TO_WORK_DIR "a73xq" "system" "bin" 0 2000 755 "u:object_r:system_file:s0"
 ADD_TO_WORK_DIR "a73xq" "system" "lib" 0 0 755 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "a73xq" "system" "lib64" 0 0 755 "u:object_r:system_lib_file:s0"
 ADD_TO_WORK_DIR "a73xq" "system" "system_ext/apex/com.android.vndk.v30.apex" 0 0 644 "u:object_r:system_file:s0"
