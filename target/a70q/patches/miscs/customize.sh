77cbf04e129de0469d06ed9cd1275a589f322912
tisenu100 <103323979+tisenu100@users.noreply.github.com>
2025-06-04 16:27:48 +0300

    a70q: Redo the SSC less patch
    Use sed to change this line to use vendor values instead of wiping the whole stack.

--- target/a70q/patches/miscs/customize.sh
+++ target/a70q/patches/miscs/customize.sh
@@ -4,6 +4,10 @@ sed -i "/# Removed by /d" "$WORK_DIR/system/system/product/etc/build.prop" \
     && sed -i "s/?=/=/g" "$WORK_DIR/system/system/product/etc/build.prop" \
     && sed -i "$(sed -n "/provisioning.hostname/=" "$WORK_DIR/system/system/product/etc/build.prop" | sed "2p;d")d" "$WORK_DIR/system/system/product/etc/build.prop"
 
+# Tip: UN1CA's SET_PROP hates the product build.prop so we instead sed the change out
+echo "Removing SSC"
+sed -i "s/persist.bluetooth.samsung.a2dp_offload.cap=sbc-aptx-ssc-hifi/persist.bluetooth.samsung.a2dp_offload.cap=sbc-aptx-hifi/g" "$WORK_DIR/system/system/product/etc/build.prop"
+
 echo "Remove unsupported permissions"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/com.sec.feature.nsflp_level_601.xml"
 DELETE_FROM_WORK_DIR "system" "system/etc/permissions/com.sec.feature.sensorhub_level40.xml"
